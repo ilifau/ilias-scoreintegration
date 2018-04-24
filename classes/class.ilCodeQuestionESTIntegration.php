0e21d03237a4902ec4f6bb184216c33e4b8d5199
Frank <frank.bauer@fau.de>
2018-04-24 09:51:17 +0200

    Method to update the manual scoring for a participant

--- classes/class.ilCodeQuestionESTIntegration.php
+++ classes/class.ilCodeQuestionESTIntegration.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once './Modules/TestQuestionPool/classes/class.assQuestion.php';
+require_once './Modules/Test/classes/inc.AssessmentConstants.php';
+require_once './Modules/TestQuestionPool/interfaces/interface.ilObjQuestionScoringAdjustable.php';
+require_once './Modules/TestQuestionPool/interfaces/interface.ilObjFileHandlingQuestionType.php';
+
 /**
  * Basic class for EST import/Export
  *
@@ -25,6 +30,74 @@ class ilCodeQuestionESTIntegration
 	{
 		$this->testObj = $a_test_obj;
 		$this->plugin = $a_plugin;
+	}
+
+	function updatePoints($active_fi, $question_fi, $pass, $reachedPoints, $maxPoints, $comment=NULL){
+		global $ilDB;
+		/*$ilDB->update("tst_solutions", array(
+			"points" => array("float", $points)
+		), array(
+			"solution_id" => array("integer", $solution_id),
+			"active_fi" => array("integer", $active_fi),
+			"question_fi" => array("integer", $question_fi)
+		));*/
+
+		/*$ilDB->update("tst_test_result", array(
+			"points" => array("float", $reachedPoints),
+			"manual" => array("integer", 1)
+		), array(
+			"pass" => array("integer", $pass),
+			"active_fi" => array("integer", $active_fi),
+			"question_fi" => array("integer", $question_fi)
+		));
+
+		if (!is_null($comment)){
+			$ilDB->update("tst_manual_fb", array(
+				"feedback" => array("float", $reachedPoints)
+			), array(
+				"pass" => array("integer", $pass),
+				"active_fi" => array("integer", $active_fi),
+				"question_fi" => array("integer", $question_fi)
+			));
+		}*/
+		
+		assQuestion::_setReachedPoints(
+			$active_fi, 
+			$question_fi, 
+			$reachedPoints, 
+			$maxPoints,
+			$pass, 
+			1, $this->testObj->areObligationsEnabled()
+		);
+
+		if (!is_null($comment)){
+			include_once "./Services/AdvancedEditing/classes/class.ilObjAdvancedEditing.php";
+
+			$feedback = ilUtil::stripSlashes(
+				$comment,
+				false, 
+				ilObjAdvancedEditing::_getUsedHTMLTagsAsString("assessment")
+			);
+			$this->testObj->saveManualFeedback(
+				$active_fi, 
+				$question_fi, 
+				$pass, 
+				$feedback
+			);
+		}
+
+		include_once "./Modules/Test/classes/class.ilObjTestAccess.php";
+		include_once("./Services/Tracking/classes/class.ilLPStatusWrapper.php");
+		ilLPStatusWrapper::_updateStatus(
+				$this->testObj->getId(), 
+				ilObjTestAccess::_getParticipantId($active_fi)
+		);
+
+		//we may have to do this only once!
+		require_once './Modules/Test/classes/class.ilTestScoring.php';
+		$scorer = new ilTestScoring($this->testObj);
+		$scorer->setPreserveManualScores(true);
+		$scorer->recalculateSolutions();
 	}
 	
 	function buildZIP($zipFile){
